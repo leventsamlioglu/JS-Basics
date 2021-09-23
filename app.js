690b02600dfc77c8503b6ae13061fa073563ca5a
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-23 16:31:36 +0200

    some arrangements

--- app.js
+++ app.js
@@ -81,3 +81,34 @@ startGameBtn.addEventListener("click", () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// not related to game
+
+const sumUp = (resultHandler, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    sum += validateNumber(num);
+  }
+  resultHandler(sum);
+};
+
+const subtractUp = function () {
+  let sum = 0;
+  for (const num of arguments) {
+    // don't use that
+    sum -= num;
+  }
+  return sum;
+};
+
+const showResult = (result) => {
+  alert("The result after adding all numbers is: " + result);
+};
+
+sumUp(showResult, 1, 5, "fdsa", -3, 6, 10);
+sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
+console.log(subtractUp(1, 10, 15, 20));
