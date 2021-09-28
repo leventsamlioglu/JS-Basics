e13d9394822ab2a0aa092cf951b431190d38cd9e
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-28 09:01:23 +0200

    concat and indexOf

--- app.js
+++ app.js
@@ -42,3 +42,15 @@ console.log(hobbies);
 
 const removedElements = hobbies.splice(-1, 1);
 console.log(hobbies);
+
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// const storedResults = testResults.slice(2);
+const storedResults = testResults.concat([3.99, 2]);
+
+testResults.push(5.91);
+
+console.log(storedResults, testResults);
+console.log(testResults.indexOf(1.5));
+
+const personData = [{ name: "Max" }, { name: "Manuel" }];
+console.log(personData.indexOf({ name: "Manuel" }));
