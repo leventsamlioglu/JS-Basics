ea0fd38457e15028f84e8833479ae862be3d444f
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-29 15:01:34 +0200

    getters and setters

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -28,7 +28,7 @@ const renderMovies = (filter = "") => {
     // getFormattedTitle = getFormattedTitle.bind(movie);
     let text = getFormattedTitle.apply(movie) + " - ";
     for (const key in info) {
-      if (key !== "title") {
+      if (key !== "title" && key !== "_title") {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -42,17 +42,22 @@ const addMovieHandler = () => {
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
 
-  if (
-    title.trim() === "" ||
-    extraName.trim() === "" ||
-    extraValue.trim() === ""
-  ) {
+  if (extraName.trim() === "" || extraValue.trim() === "") {
     return;
   }
 
   const newMovie = {
     info: {
-      title,
+      set title(val) {
+        if (val.trim() === "") {
+          this._title = "DEFAULT";
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
@@ -61,6 +66,9 @@ const addMovieHandler = () => {
       return this.info.title.toUpperCase();
     },
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
