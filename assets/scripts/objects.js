8fbc3bc106b2bb37b5c4695c076e68199065d3cf
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-29 08:36:26 +0200

    dynamic access and assignment

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,13 +3,17 @@ const movieList = document.getElementById("movie-list");
 movieList.style["background-color"] = "red";
 movieList.style.display = "block";
 
+const userChosenKeyName = "level";
+
 let person = {
   "first name": "Max",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  [userChosenKeyName]: "...",
   greet: function () {
     alert("Hi there!");
   },
+  1.5: "hello",
 };
 
 // ...
@@ -20,4 +24,8 @@ delete person.age;
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person["first name"]);
+const keyName = "first name";
+
+console.log(person[keyName]);
+console.log(person[1.5]);
+console.log(person);
