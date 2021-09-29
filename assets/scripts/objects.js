211cd8cc028bf06384e78377a49b3ef2fe3ea6f6
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-29 08:26:03 +0200

    square bracket

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,5 +1,10 @@
+const movieList = document.getElementById("movie-list");
+
+movieList.style["background-color"] = "red";
+movieList.style.display = "block";
+
 let person = {
-  name: "Max",
+  "first name": "Max",
   age: 30,
   hobbies: ["Sports", "Cooking"],
   greet: function () {
@@ -15,4 +20,4 @@ delete person.age;
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person);
+console.log(person["first name"]);
