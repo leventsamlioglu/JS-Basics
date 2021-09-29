9fa538a080c527ae806206f221255d5901971c00
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-29 09:01:26 +0200

    demo app - rendering movies

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,31 +1,49 @@
-const movieList = document.getElementById("movie-list");
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
-movieList.style["background-color"] = "red";
-movieList.style.display = "block";
+const movies = [];
 
-const userChosenKeyName = "level";
+const renderMovies = () => {
+  const movieList = document.getElementById("movie-list");
 
-let person = {
-  "first name": "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  [userChosenKeyName]: "...",
-  greet: function () {
-    alert("Hi there!");
-  },
-  1.5: "hello",
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
 };
 
-// ...
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
 
-// person.age = 31;
-delete person.age;
-// person.age = undefined;
-// person.age = null;
-person.isAdmin = true;
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
 
-const keyName = "first name";
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
 
-console.log(person[keyName]);
-console.log(person[1.5]);
-console.log(person);
+  movies.push(newMovie);
+  renderMovies();
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
