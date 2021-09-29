65de304d089633811732186c81d3df2f99406f9f
leventsamlioglu <leventsamlioglu@icloud.com>
2021-09-29 13:50:55 +0200

    this keyword

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -22,8 +22,9 @@ const renderMovies = (filter = "") => {
     const movieEl = document.createElement("li");
     const { info, ...otherProps } = movie;
     console.log(otherProps);
-    const { title: movieTitle } = info;
-    let text = movieTitle + " - ";
+    // const { title: movieTitle } = info;
+    // const { getFormattedTitle } = movie;
+    let text = movie.getFormattedTitle() + " - ";
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
@@ -53,6 +54,9 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
+    getFormattedTitle: function () {
+      return this.info.title.toUpperCase();
+    },
   };
 
   movies.push(newMovie);
