e1803c649978912843a8a3896866d5fb98ea8619
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-06 13:39:29 +0200

    property methods bind

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ class AgedPerson {
   }
 }
 
-class Person extends AgedPerson {
+class Person {
   name = "Max";
 
   constructor() {
-    super();
+    // super();
     this.age = 30;
+    // this.greet = function() { ... }
   }
+
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
 
   greet() {
     console.log(
@@ -22,12 +29,14 @@ class Person extends AgedPerson {
 // function Person() {
 //   this.age = 30;
 //   this.name = 'Max';
-//   this.greet = function() {
-//     console.log(
-//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-//     );
-//   };
+//   // this.greet = function() { ... };
 // }
+
+// Person.prototype.greet = function() {
+//   console.log(
+//     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//   );
+// };
 
 // Person.describe = function() {
 //   console.log('Creating persons...');
@@ -55,4 +64,8 @@ class Person extends AgedPerson {
 
 const p = new Person();
 const p2 = new Person();
-console.log(p.__proto__ === p.__proto__);
+p.greet();
+console.log(p);
+
+const button = document.getElementById("btn");
+button.addEventListener("click", p.greet.bind(p));
