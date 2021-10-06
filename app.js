bcb7552ed611deb65e56f2d13a469cd278274dee
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-06 13:38:44 +0200

    classes methods prototype

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
 
-// class Person extends AgedPerson {
-//   name = 'Max';
+class Person extends AgedPerson {
+  name = "Max";
 
-//   constructor() {
-//     super();
-//     this.age = 30;
-//   }
+  constructor() {
+    super();
+    this.age = 30;
+  }
 
-//   greet() {
-//     console.log(
-//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-//     );
-//   }
-// }
-
-function Person() {
-  this.age = 30;
-  this.name = "Max";
-  this.greet = function () {
+  greet() {
     console.log(
       "Hi, I am " + this.name + " and I am " + this.age + " years old."
     );
-  };
+  }
 }
 
-Person.describe = function () {
-  console.log("Creating persons...");
-};
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+//   this.greet = function() {
+//     console.log(
+//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//     );
+//   };
+// }
+
+// Person.describe = function() {
+//   console.log('Creating persons...');
+// }
 
 // Person.prototype = {
 //   printAge() {
@@ -39,16 +39,20 @@ Person.describe = function () {
 //   }
 // };
 
-Person.prototype.printAge = function () {
-  console.log(this.age);
-};
+// Person.prototype.printAge = function() {
+//   console.log(this.age);
+// };
 
-console.dir(Person);
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
 
 const p = new Person();
-p.greet();
-p.printAge();
-console.log(p.length);
-console.log(p.toString());
-const p2 = new p.__proto__.constructor();
-console.dir(Object.prototype.__proto__);
+const p2 = new Person();
+console.log(p.__proto__ === p.__proto__);
