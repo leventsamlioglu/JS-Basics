da300f5b8c868ba79d7724b9178519ca73b72605
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-13 15:17:08 +0200

    factory functions

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
-function add(num1, num2) {
-  return num1 + num2;
+// // Pure Functions
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// console.log(add(1, 5)); // 6
+// console.log(add(12, 15)); // 27
+
+// function addRandom(num1) {
+//   return num1 + Math.random();
+// }
+
+// console.log(addRandom(5));
+
+// // Impure Functions
+
+// let previousResult = 0;
+
+// function addMoreNumbers(num1, num2) {
+//   const sum = num1 + num2;
+//   previousResult = sum; // Side effect
+//   return sum;
+// }
+
+// console.log(addMoreNumbers(1, 5));
+
+// const hobbies = ["Sports", "Cooking"];
+
+// function printHobbies(h) {
+//   h.push("NEW HOBBY"); // Side effect
+//   console.log(h);
+// }
+
+// printHobbies(hobbies);
+
+// Factory Functions
+
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+  return calculateTax;
 }
 
-// function sendDataToServer() {}
+const claculateVatAmount = createTaxCalculator(0.19);
+const claculateIncomeTaxAmount = createTaxCalculator(0.25);
 
-console.log(add(1, 5)); // 6
-console.log(add(12, 15)); // 27
-
-function addRandom(num1) {
-  return num1 + Math.random();
-}
-
-console.log(addRandom(5));
-
-let previousResult = 0;
-
-function addMoreNumbers(num1, num2) {
-  const sum = num1 + num2;
-  previousResult = sum;
-  return sum;
-}
-
-console.log(addMoreNumbers(1, 5));
-
-const hobbies = ["Sports", "Cooking"];
-
-function printHobbies(h) {
-  h.push("NEW HOBBY");
-  console.log(h);
-}
-
-printHobbies(hobbies);
+console.log(claculateVatAmount(100));
+console.log(claculateIncomeTaxAmount(200));
