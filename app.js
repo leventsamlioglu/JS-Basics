f6a45778256ce0234b2329a7a7a61da81539085b
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-25 16:12:26 +0200

    working with cookies

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 const storeBtn = document.getElementById("store-btn");
 const retrBtn = document.getElementById("retrieve-btn");
 
-const userId = "u123";
-const user = {
-  name: "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-};
-
 storeBtn.addEventListener("click", () => {
-  sessionStorage.setItem("uid", userId);
-  localStorage.setItem("user", JSON.stringify(user));
+  const userId = "u123";
+  const user = { name: "Max", age: 30 };
+  document.cookie = `uid=${userId}; max-age=360`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener("click", () => {
-  const extractedId = sessionStorage.getItem("uid");
-  const extractedUser = JSON.parse(localStorage.getItem("user"));
-  console.log(extractedUser);
-  if (extractedId) {
-    console.log("Got the id - " + extractedId);
-  } else {
-    console.log("Could not find id.");
-  }
+  console.log(document.cookie);
+  const cookieData = document.cookie.split(";");
+  const data = cookieData.map((i) => {
+    return i.trim(); // removes white space
+  });
+  console.log(data[1].split("=")[1]); // user value
 });
