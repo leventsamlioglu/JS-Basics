4732b2ea248ac90f528334e875c5f11f66ed9625
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-27 17:40:09 +0200

    add output comments

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -2,26 +2,59 @@ export class Modal {
   constructor(contentId, fallbackText) {
     this.fallbackText = fallbackText;
     this.contentTemplateEl = document.getElementById(contentId);
-    this.modalTemplateEl = document.getElementById('modal-template');
+    // <template id="loading-modal-content">
+    //   #document-fragment
+    //   <div class="modal__content centered">
+    //     <div class="lds-dual-ring"></div>
+    //   </div>
+    // </template>
+    this.modalTemplateEl = document.getElementById("modal-template");
+    // <template id="modal-template">
+    //   #document-fragment
+    //   <div class="backdrop"></div>
+    //   <div class="modal"></div>
+    // </template>
   }
 
   show() {
-    if ('content' in document.createElement('template')) {
+    if ("content" in document.createElement("template")) {
       const modalElements = document.importNode(
         this.modalTemplateEl.content,
         true
+        // #document-fragment
+        // <div class="backdrop"></div>
+        // <div class="modal"></div>
+
+        // modalElements = #document-fragment
       );
-      const modalElement = modalElements.querySelector('.modal');
-      const backdropElement = modalElements.querySelector('.backdrop');
+
+      const modalElement = modalElements.querySelector(".modal");
+      // <div class="modal">
+      //   <div class="modal__content centered">
+      //     <div class="lds-dual-ring"></div>
+      //   </div>
+      // </div>
+
+      const backdropElement = modalElements.querySelector(".backdrop");
+      //  <div class="backdrop"></div>
+
       const contentElement = document.importNode(
         this.contentTemplateEl.content,
         true
       );
+      // this.contentTemplateEl.content =
+      // #document-fragment
+      // <div class="modal__content centered">
+      //   <div class="lds-dual-ring"></div>
+      // </div>
+
+      // contentElement =
+      // #document-fragment
 
       modalElement.appendChild(contentElement);
 
-      document.body.insertAdjacentElement('afterbegin', modalElement);
-      document.body.insertAdjacentElement('afterbegin', backdropElement);
+      document.body.insertAdjacentElement("afterbegin", modalElement);
+      document.body.insertAdjacentElement("afterbegin", backdropElement);
     } else {
       // fallback code
       alert(this.fallbackText);
