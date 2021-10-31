37c3f5b0a6819157d84fcdbdc82901738573dcfd
leventsamlioglu <leventsamlioglu@icloud.com>
2021-10-31 18:42:15 +0100

    iterators

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// SYMBOLS
+
 // Library land
 const uid = Symbol();
 console.log(uid);
@@ -19,3 +21,32 @@ user.id = "p2"; // this should not be possible!
 console.log(user[Symbol("uid")]);
 console.log(Symbol("uid") === Symbol("uid"));
 console.log(user.toString());
+
+// ITERATORS
+
+const company = {
+  curEmployee: 0,
+  employees: ["Max", "Manu", "Anna"],
+  next() {
+    if (this.curEmployee >= this.employees.length) {
+      return { value: this.curEmployee, done: true };
+    }
+    const returnValue = {
+      value: this.employees[this.curEmployee],
+      done: false,
+    };
+    this.curEmployee++;
+    return returnValue;
+  },
+};
+
+// console.log(company.next());
+// console.log(company.next());
+// console.log(company.next());
+
+let employee = company.next();
+
+while (!employee.done) {
+  console.log(employee.value);
+  employee = company.next();
+}
