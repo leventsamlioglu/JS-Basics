658b6363b33a775d4da5b7f3b20b24416e9e4071
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-06 15:19:44 +0100

    object array function types

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+var num1Input = document.getElementById('num1');
+var num2Input = document.getElementById('num2'); //<HTMLInputElement>
+var buttonElement = document.querySelector('button');
 function add(a, b) {
     return a + b;
 }
-var result = add(5, 3);
-console.log(result);
+// function printResult(result) {
+//   console.log(result);
+// }
+// const result = add(5, 3);
+// let isDone = false;
+// printResult(result);
+var results = [];
+var names = ['Max'];
+buttonElement.addEventListener('click', function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = add(num1, num2);
+    var resultContainer = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    // results.push(5);
+    results[0].print();
+});
