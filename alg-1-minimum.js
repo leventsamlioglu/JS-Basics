7fd8e16c16c478842751489a4151fedfd6f1886d
leventsamlioglu <leventsamlioglu@icloud.com>
2021-11-06 08:57:46 +0100

    time complexity

--- alg-1-minimum.js
+++ alg-1-minimum.js
@@ -1,17 +1,19 @@
-function getMin(numbers) {
-  if (!numbers.length) {
+function getMin(numbers) { // [3, 1, 2]
+  if (!numbers.length) { // 1 execution
     throw new Error('Should not be an empty array!');
   }
-  let currentMinimum = numbers[0];
+  let currentMinimum = numbers[0]; // 1 execution
 
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] < currentMinimum) {
-      currentMinimum = numbers[i];
+  for (let i = 1; i < numbers.length; i++) { // 1 execution
+    if (numbers[i] < currentMinimum) { // 2 executions
+      currentMinimum = numbers[i]; // 1 execution
     }
   }
 
-  return currentMinimum;
+  return currentMinimum; // 1 execution
 }
+
+// T = n => Linear Time Complexity => O(n)
 
 function getMin2(numbers) {
   if (!numbers.length) {
@@ -19,9 +21,9 @@ function getMin2(numbers) {
   }
 
   for (let i = 0; i < numbers.length; i++) {
-    let outerElement = numbers[i];
+    let outerElement = numbers[i]; // n times
     for (let j = i + 1; j < numbers.length; j++) {
-      let innerElement = numbers[j];
+      let innerElement = numbers[j]; // n times
 
       if (outerElement > innerElement) {
         // swap
@@ -37,8 +39,10 @@ function getMin2(numbers) {
   return numbers[0];
 }
 
+// Quadratic Time Complexity => n * n => O(n^2)
+
 const testNumbers = [5, 1, 5];
 
-const min = getMin2(testNumbers);
+const min = getMin(testNumbers);
 
 console.log(min);
